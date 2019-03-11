b5612e6394591d20894c895c8d27495b027bab20
KCetera <kcetera@ucsc.edu>
2019-03-11 14:49:45 -0700

    World&Animations
    Created the world for the player to explore and added a few base animations to the player character with a controller

---
+++ Cetera_Kaylie_CMPM121FinalAssignment/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform cam;
+    Vector2 input;
+    public int speed;
+
+    public GameObject testParticle;
+    public Vector3 particleOffset;
+
+    void Start()
+    {
+  
+    }
+
+
+    void FixedUpdate()
+    {
+        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        input = Vector2.ClampMagnitude(input, 1) * speed;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down * Time.deltaTime * 100);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * 100);
+        }
+
+        Vector3 camF = cam.forward;
+        Vector3 camR = cam.right;
+        camF.y = 0;
+        camR.y = 0;
+        camF = camF.normalized;
+        camR = camR.normalized;
+
+        transform.position += (camF * input.y + camR * input.x) * Time.deltaTime * 5;
+    }
+
+}
